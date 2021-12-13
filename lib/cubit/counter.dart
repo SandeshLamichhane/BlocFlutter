f0e9b3a9089107b1d3df4aabf4a4233cd2356269
SandeshLamichhane <sc05213061@gmail.com>
2021-12-13 11:30:12 +0545

    Bloc provider and listener

--- lib/cubit/counter.dart
+++ lib/cubit/counter.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutterblock/cubit/cubit/counter_cubit.dart';
+import 'package:flutterblock/main.dart';
 
 class CounterView extends StatefulWidget {
   const CounterView({Key? key}) : super(key: key);
@@ -18,7 +19,16 @@ class _CounterViewState extends State<CounterView> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            BlocBuilder<CounterCubit, CounterState>(
+            BlocConsumer<CounterCubit, CounterState>(
+
+             listener: (context, state) {     
+            if(state.wasIncremented==true ||state.wasIncremented==false){
+              SnackBar snackBar=  
+              SnackBar(content:Text(state.counterValue.toString()+" Welcome to the next world.") );
+              ScaffoldMessenger.of(context).showSnackBar(snackBar);
+          }
+
+          },
               builder: (context, state) {
                 return Text(state.counterValue.toString());
               },
@@ -26,16 +36,20 @@ class _CounterViewState extends State<CounterView> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                FloatingActionButton(onPressed: () {
-                  BlocProvider.of<CounterCubit>(context).decrement();
-                },
-                 child: Text("Sub"),
+                FloatingActionButton(
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).decrement();
+                  },
+                  child: Text("Sub"),
                 ),
-                SizedBox(width: 20,),
-                FloatingActionButton(onPressed: () {
-                  BlocProvider.of<CounterCubit>(context).increment();
-                },
-                child: Text("Add"),
+                SizedBox(
+                  width: 20,
+                ),
+                FloatingActionButton(
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).increment();
+                  },
+                  child: Text("Add"),
                 )
               ],
             )
--- lib/cubit/cubit/counter_state.dart
+++ lib/cubit/cubit/counter_state.dart
@@ -4,10 +4,12 @@ part of 'counter_cubit.dart';
 
  class CounterState{
   int counterValue;
+  bool? wasIncremented;
 
  CounterState({
-  required  this.counterValue
+  required  this.counterValue,
+    this.wasIncremented
  });
- 
+
 
  }
